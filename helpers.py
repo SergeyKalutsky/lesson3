891e529f8ebbb79bf2d950695f73f35f0469bb6d
Serget <skalytesky@gmail.com>
2021-03-26 18:05:09 +0300

    added changes for image showing

--- helpers.py
+++ helpers.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 
 def display_images(columns, rows, images, figsize=(27, 10)):
-    fig=plt.figure(figsize=(27, 10))
+    fig=plt.figure(figsize=(20, 10))
     for i in range(1, columns*rows+1):
         fig.add_subplot(rows, columns, i)
         plt.imshow(images[i-1])
